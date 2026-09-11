8ef80f80d53befb1d0cd409fddf4c95b8c897c0a
6108 <6108@users.noreply.github.com>
2026-09-11 22:18:24 +0900

    style: 관심 종목 화면 UI 추가

---
+++ lib/screens/watchlist_screen.dart
@@ -0,0 +1,17 @@
+import 'package:edencrew_assignment_starter/widgets/watchlist_header.dart';
+import 'package:flutter/material.dart';
+import '../theme/theme.dart';
+
+class WatchlistScreen extends StatelessWidget {
+  const WatchlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.colors;
+
+    return Scaffold(
+      backgroundColor: colors.surfaceBase,
+      body: SafeArea(child: Column(children: [const WatchlistHeader()])),
+    );
+  }
+}
