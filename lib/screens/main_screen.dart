a852b8dc10c6172f56efd4455d057703fd3d1072
6108 <6108@users.noreply.github.com>
2026-09-11 22:45:26 +0900

    feat: 메인 스크린 갱신
    - 앱 진입 시 main_screen을 보여줌
    - 임시 검색 화면 추가

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,32 @@
+import 'package:edencrew_assignment_starter/screens/search_screen.dart';
+import 'package:edencrew_assignment_starter/screens/watchlist_screen.dart';
+import 'package:edencrew_assignment_starter/widgets/bottom_nav.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int currentIndex = 0;
+
+  final screens = const [WatchlistScreen(), SearchScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: currentIndex, children: screens),
+      bottomNavigationBar: BottomNav(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
